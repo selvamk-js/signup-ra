5ff59febce81fa607943a8be4476039c95a035b2
Selvam Kumar <selvamk@cnxsi.com>
2023-04-26 17:42:48 +0530

    initial commit

---
+++ src/Login/index.tsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import "./Login.css";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  passwordAgain: "",
+  terms: false,
+};
+const reducer = (state: any, action: any) => {
+  // if(action.input === 'name'){
+  return { ...state, [action.input]: action.value };
+};
+
+function Login() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const validateState = () => {
+    let enable = false;
+    if (
+      state.password !== "" &&
+      state.passwordAgain !== "" &&
+      state.password === state.passwordAgain &&
+      state.password.length > 3 &&
+      state.email &&
+      state.terms
+    ) {
+      enable = true;
+    }
+    console.log(enable);
+    return enable;
+  };
+
+  // console.log(state);
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    alert(`Hello ${state.name} you have successfully loggedin.....!!`);
+  };
+
+  const onChange = (e: any) => {
+    const { name, value, checked } = e.target;
+    const action = {
+      input: name,
+      value: name === "terms" ? checked : value,
+    };
+    dispatch(action);
+  };
+
+  return (
+    <div className="App">
+      <div className="LoginFormContainer">
+        <h2 className="LoginFormHeading">Sign Up</h2>
+        <form className="LoginForm">
+          <input
+            className="textInput"
+            type="text"
+            name="name"
+            required
+            placeholder="Enter your name"
+            onChange={onChange}
+          />
+          <input
+            className="textInput"
+            type="text"
+            name="email"
+            required
+            placeholder="Enter your email"
+            onChange={onChange}
+          />
+          <input
+            className="textInput"
+            type="password"
+            name="password"
+            required
+            placeholder="Enter your password"
+            onChange={onChange}
+          />
+          <input
+            className="textInput"
+            type="password"
+            name="passwordAgain"
+            required
+            placeholder="Confirm your password"
+            onChange={onChange}
+          />
+          <label className="CheckboxLabel">
+            <input
+              className="CheckboxInput"
+              type="checkbox"
+              name="terms"
+              onChange={onChange}
+            />
+            Accept the terms and condition
+          </label>
+          <button
+            className={"loginButton"}
+            // onClick={handleClick}
+            disabled={!validateState()}
+          >
+            Register
+          </button>
+          {/* <Navigate to="/home">Login</Navigate> */}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
